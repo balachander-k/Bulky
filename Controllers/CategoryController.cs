6586ccdf3ac3878458ff5c7859910d4f89bdbe5a
balachander-k <chanderbala687@gmail.com>
2025-02-27 20:07:12 +0530

    Category Controller Changed

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bulky.Data;
+using Bulky.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bulky.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList = _db.Catgories.ToList();
+            return View(objCategoryList);
         }
     }
 }
